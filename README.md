2a92f8fc4a9c5951cd347db3213fd1264483139f
wangkemiao <wangkemiao@baidu.com>
2015-04-27 10:27:13 +0800

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # k-component-element
 component element based on k-component
+
+this is a set of defined basic component elements that we can use directly.
---
+++ src/main.js
@@ -0,0 +1,19 @@
+/**
+ * @file component element using k-component
+ *
+ * @author Leo Wang(leowang721@gmail.com)
+ */
+
+define(function (require) {
+
+    /**
+     * component element using k-component
+     * @class component.element
+     * @type {Object}
+     */
+    var componentElement = {
+        version: '0.0.1-alpha.1'
+    };
+
+    return componentElement;
+});
